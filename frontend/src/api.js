d1044addb623ee1895449ab57a109c9334e44013
ReversiblWizard <Sayakmitra16@gmail.com>
2025-08-01 12:08:50 +0530

    Update backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/django-react-task-app/backend/v1"
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,   // "http://127.0.0.1:8000"
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,   // "http://127.0.0.1:8000"
 });
 
 api.interceptors.request.use(
